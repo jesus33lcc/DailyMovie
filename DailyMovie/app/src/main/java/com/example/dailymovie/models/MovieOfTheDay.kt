5c030125c46685b6764440437995899bff40e958
jesus33lcc <jesus33lcc@gmail.com>
2024-06-02 20:57:06 +0200

    Diseño Casi Completo

--- DailyMovie/app/src/main/java/com/example/dailymovie/models/MovieOfTheDay.kt
+++ DailyMovie/app/src/main/java/com/example/dailymovie/models/MovieOfTheDay.kt
@@ -3,7 +3,6 @@ package com.example.dailymovie.models
 data class MovieOfTheDay(
     val id: Int,
     val title: String,
-    val rating: Double,
     val review: String,
     val date: String,
     val author: String,
